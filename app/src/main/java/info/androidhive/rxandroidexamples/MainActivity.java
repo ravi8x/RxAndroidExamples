7ec36a967d6c38d5b46cb15cae7d404f2d18cca2
Ravi Tamada <ravi8x@gmail.com>
2018-01-27 20:28:40 +0530

    New RxAndroid project

---
+++ app/src/main/java/info/androidhive/rxandroidexamples/MainActivity.java
@@ -0,0 +1,13 @@
+package info.androidhive.rxandroidexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/info/androidhive/rxandroidexamples/basics/Example1Activity.java
@@ -0,0 +1,15 @@
+package info.androidhive.rxandroidexamples.basics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.rxandroidexamples.R;
+
+public class Example1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example1);
+    }
+}
